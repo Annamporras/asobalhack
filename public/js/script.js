f8dc9f8b6e31f833fc8fa60aa3eb3239da12cb45
Eduardo Gordillo <edu.m.gordillo@gmail.com>
2022-02-26 18:18:15 +0100

    repair iframe fail

--- public/js/script.js
+++ public/js/script.js
@@ -6,10 +6,3 @@ document.addEventListener(
   false
 );
 
-const myFrame = document.querySelector('#myFrame')
-
-var blob = new Blob(['<div>Hello World</div>'], { type: 'text/html' });
-
-var url = URL.createObjectURL(blob);
-
-iframe.src = url;
